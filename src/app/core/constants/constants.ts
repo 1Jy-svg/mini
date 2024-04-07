7595fabbd202d5f09c3e7f68e9aa8e01aa83d4d3
1Jy-svg <jyothikasankar2@gmail.com>
2024-04-07 20:07:46 +0530

    jjj

---
+++ src/app/core/constants/constants.ts
@@ -0,0 +1,23 @@
+export const firebaseConfig = {
+
+
+    apiKey: "AIzaSyC71tIdYXEzqhzx2jAubfRdz5mqfaccVbE",
+  
+    authDomain: "angular-courier.firebaseapp.com",
+  
+    projectId: "angular-courier",
+  
+    storageBucket: "angular-courier.appspot.com",
+  
+    messagingSenderId: "766541741485",
+  
+    appId: "1:766541741485:web:3d1db62ec65957aca53524"
+  
+  };
+  
+  
+  
+  
+  
+
+  
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule,NgClass,RouterLink,ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  tform!:FormGroup;
+  constructor(private fb: FormBuilder,private http:HttpClient,private router: Router,private authService:AuthService){
+  this.tform=this.fb.nonNullable.group({
+    username:['',Validators.required],
+    email:['',Validators.required],
+    password:['',Validators.required],
+  });}
+  errorMessage: string | null =null;
+  onSignup():void{
+    const rawForm=this.tform.getRawValue();
+    this.authService.register(rawForm.email,rawForm.username,rawForm.password)
+    .subscribe({
+      next: () => {
+      this.router.navigateByUrl('/login');
+      },
+      error :(err) =>{
+        alert('Invalid username or password');
+      }
+    });
+  }
+  onlogin():void{
+    const rawForm=this.tform.getRawValue();
+    this.authService.login(rawForm.email,rawForm.password)
+    .subscribe({
+      next:()=>{
+        alert('login successful');
+      this.router.navigateByUrl('/userdetails');
+    }
+  });
+  }
+
+}
